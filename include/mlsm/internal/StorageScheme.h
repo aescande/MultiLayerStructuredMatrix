a447a69779be3ba2336099754f1c3db2476b2d35
Adrien Escande <adrien.escande@aist.go.jp>
2021-12-21 19:18:14 +0900

    Iterators for StorageScheme

--- include/mlsm/internal/StorageScheme.h
+++ include/mlsm/internal/StorageScheme.h
@@ -6,6 +6,7 @@
 
 #include <assert.h>
 #include <utility>
+#include <vector>
 
 namespace mls::internal
 {
@@ -19,17 +20,73 @@ enum class SymmetricStorage
 class StorageScheme
 {
 public:
-  using Row = Line<int, StorageScheme, true>;
-  using Col = Line<int, StorageScheme, false>;
+  struct LineIterValue
+  {
+    int i = -1;      // Place of the element in the line
+    int idx = 0;     // Index of the element in the storage
+    bool tr = false; // Need to transpose the element
+
+    friend bool operator==(const LineIterValue & lhs, const LineIterValue & rhs)
+    {
+      return std::tie(lhs.i, lhs.idx, lhs.tr) == std::tie(rhs.i, rhs.idx, rhs.tr);
+    }
+  };
+
+  template<bool Row>
+  class LineIterator : public Line<LineIterValue, StorageScheme, Row>::UnderlyingIt
+  {
+  public:
+    using Base = typename Line<LineIterValue, StorageScheme, Row>::UnderlyingIt;
+    LineIterator(int l, const StorageScheme & s)
+    : Base(l), shapeIt_(shapeLine(l, s).begin()), shapeItEnd_(shapeLine(l, s).end()), scheme_(s)
+    {
+      assign();
+    }
+    LineIterValue val() const override { return v_; }
+    void next() override
+    {
+      if(++shapeIt_ != shapeItEnd_)
+        assign();
+      else
+        v_ = {};
+    };
+
+  private:
+    const auto & shapeLine(int l, const StorageScheme & s)
+    {
+      if constexpr(Row)
+        return s.shape().row(l);
+      else
+        return s.shape().col(l);
+    }
+    void assign()
+    {
+      v_.i = *shapeIt_;
+      if constexpr(Row)
+        std::tie(v_.idx, v_.tr) = scheme_.index(line(), v_.i);
+      else
+        std::tie(v_.idx, v_.tr) = scheme_.index(v_.i, line());
+    }
+    typename ShapeBase::Line<Row>::Iterator shapeIt_;
+    typename ShapeBase::Line<Row>::Iterator shapeItEnd_;
+    const StorageScheme & scheme_;
+    LineIterValue v_;
+  };
+
+  using Row = Line<LineIterValue, StorageScheme, true>;
+  using Col = Line<LineIterValue, StorageScheme, false>;
+  using RowIterator = LineIterator<true>;
+  using ColIterator = LineIterator<false>;
 
   StorageScheme(SymmetricStorage symmetric) : shape_(nullptr), symmetric_(symmetric) {}
 
   void setShape(const ShapeBase & shape)
   {
-    assert(!isSymmetric() || shape_->rows() == shape_->cols());
+    assert(!isSymmetric() || shape.rows() == shape.cols());
     shape_ = &shape;
     processShape_();
   }
+  const ShapeBase & shape() const { return *shape_; }
 
   /** Return true if stored.*/
   virtual bool isStored(int r, int c) const
@@ -52,8 +109,8 @@ public:
   Row row(int r) const { return {*this, r}; }
   Col col(int c) const { return {*this, c}; }
 
-  virtual typename Row::UnderlyingItPtr rowIterator(int r) const = 0;
-  virtual typename Col::UnderlyingItPtr colIterator(int c) const = 0;
+  virtual typename Row::UnderlyingItPtr rowIterator(int r) const { return std::make_unique<RowIterator>(r, *this); }
+  virtual typename Col::UnderlyingItPtr colIterator(int c) const { return std::make_unique<ColIterator>(c, *this); }
 
 protected:
   bool isSymmetric() const { return symmetric_ != SymmetricStorage::None; }
@@ -68,65 +125,15 @@ protected:
 class DenseStorageScheme : public StorageScheme
 {
 public:
-  class RowIterator : public LineIteratorBase<int>
-  {
-  public:
-    RowIterator(int r, const DenseShape & s) : LineIteratorBase(r), v_(r), s_(s), last_((s_.cols() - 1) * s_.rows()) {}
-    int val() const override { return v_; }
-    void next() override
-    {
-      if(v_ != -1)
-      {
-        if(v_ < last_)
-          v_ += s_.rows();
-        else
-          v_ = -1;
-      }
-    };
-
-  private:
-    int v_;
-    const DenseShape & s_;
-    int last_;
-  };
-
-  class ColIterator : public LineIteratorBase<int>
-  {
-  public:
-    ColIterator(int c, const DenseShape & s) : LineIteratorBase(c), v_(c * s.rows()), s_(s) {}
-    int val() const override { return v_; }
-    void next() override
-    {
-      if(v_ != -1)
-      {
-        if(v_ < s_.cols() - 1)
-          ++v_;
-        else
-          v_ = -1;
-      }
-    };
-
-  private:
-    int v_;
-    const DenseShape & s_;
-  };
   DenseStorageScheme() : StorageScheme(SymmetricStorage::None) {}
   int size() const override { return shape_->cols() * shape_->rows(); }
-  typename Row::UnderlyingItPtr rowIterator(int r) const override
-  {
-    return typename Row::UnderlyingItPtr(new RowIterator(r, shape()));
-  }
-  typename Col::UnderlyingItPtr colIterator(int c) const override
-  {
-    return typename Col::UnderlyingItPtr(new ColIterator(c, shape()));
-  }
 
 protected:
   void processShape_() override { assert(dynamic_cast<const DenseShape *>(shape_)); }
 
   bool isStored_(int r, int c) const override { return true; };
 
-  std::pair<int, bool> index_(int r, int c) const override { return {c * shape_->cols() + r, false}; }
+  std::pair<int, bool> index_(int r, int c) const override { return {c * shape_->rows() + r, false}; }
 
 private:
   const DenseShape & shape() const { return static_cast<const DenseShape &>(*shape_); }
@@ -145,7 +152,7 @@ private:
  * |  0   0  B13 B24 B35 B46 B57 |
  * |  0  B12 B23 B34 B45 B56  0  |
  * | B11 B22 B33 B44 B55  0   0  |
- * | B21 B32 B43 B45  0   0   0  |
+ * | B21 B32 B43 B54  0   0   0  |
  * and represented in a column-major way i.e
  * (0, 0, B12, B21, 0, B12, B22, B32, B13, B23, B33, B43, B24, ...)
  *
@@ -158,6 +165,9 @@ class BandStorageScheme : public StorageScheme
 public:
   BandStorageScheme(SymmetricStorage s = SymmetricStorage::None) : StorageScheme(s) {}
   int size() const override { return (cn_ - c0_) * b_; }
+  int effectiveBandwidth() const { return b_; }
+  int effectiveLowerBandwidth() const { return le_; }
+  int effectiveUpperBandwidth() const { return ue_; }
 
 protected:
   void processShape_() override
@@ -165,14 +175,21 @@ protected:
     assert(dynamic_cast<const BandShape *>(shape_));
     const auto & s = shape();
     assert((!isSymmetric() || s.lowerBandwidth() == s.upperBandwidth())
-           && "Symmetric band matrix should have the same lower and upper bandwidth");
+           && "Symmetric band matrix should have the same lower and upper bandwidth.");
+    assert((!isSymmetric() || s.rows() == s.cols()));
 
-    c0_ = std::max(0, -int(s.lowerBandwidth()));
-    cn_ = std::min(s.cols(), s.rows() + int(s.upperBandwidth()));
-    if(isSymmetric())
-      b_ = int(shape().upperBandwidth()) + 1;
-    else
-      b_ = int(shape().bandwidth());
+    int r = s.rows();
+    int c = s.cols();
+    int lm = s.lowerBandwidth().toInt(s.maxDim());
+    int um = s.upperBandwidth().toInt(s.maxDim());
+    // Effective lower and upper bandwidth
+    le_ = (symmetric_ == SymmetricStorage::Upper) ? 0 : std::clamp(lm, - c, r - 1);
+    ue_ = (symmetric_ == SymmetricStorage::Lower) ? 0 : std::clamp(um, - r, c - 1);
+    assert(!isSymmetric() || (le_ >= 0 && ue_ >= 0));
+
+    c0_ = std::max(0, -le_);
+    cn_ = std::min(s.cols(), s.rows() + ue_);
+    b_ = le_ + ue_ + 1;
   }
 
   bool isStored_(int r, int c) const override
@@ -195,20 +212,23 @@ protected:
     if(!shape().isNonZero(r, c))
       return {-1, false};
 
+    // We have that c-u <= r <= c+l. This is equivalent to 0 <= r-c+u <= l+u
+    // We make the "coordinate change" (r, c) -> (r-c+u, c-c0)
+    auto idx = [this](int r, int c) { return r - c + ue_ + (c - c0_) * b_; };
     switch(symmetric_)
     {
       case SymmetricStorage::None:
-        return {r + (c - c0_) * b_, false};
+        return {idx(r, c), false};
       case SymmetricStorage::Lower:
         if(r >= c)
-          return {r + c * b_, false};
+          return {idx(r, c), false};
         else
-          return {c + r * b_, true};
+          return {idx(c, r), true};
       case SymmetricStorage::Upper:
         if(r <= c)
-          return {r + c * b_, false};
+          return {idx(r, c), false};
         else
-          return {c + r * b_, true};
+          return {idx(c, r), true};
       default:
         assert(false);
     }
@@ -217,9 +237,11 @@ protected:
 private:
   const BandShape & shape() const { return static_cast<const BandShape &>(*shape_); }
 
-  int c0_; // first non-zero column
-  int cn_; // first zero column after the non-zero ones
-  int b_;  // effective bandwith from the storage viewpoint
+  int c0_ = -1; // first non-zero column
+  int cn_ = -1; // first zero column after the non-zero ones
+  int b_ = -1;  // effective bandwith from the storage viewpoint
+  int le_ = 0;
+  int ue_ = 0;
 };
 
 } // namespace mls::internal
--- tests/StorageSchemeTest.cpp
+++ tests/StorageSchemeTest.cpp
@@ -2,11 +2,174 @@
 
 #include <mlsm/internal/StorageScheme.h>
 
+#include <Eigen/Core>
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
 #include "doctest/doctest.h"
 
 using namespace mls::internal;
+using LIV = StorageScheme::LineIterValue;
+
+// Generate a r x c band matrix with lower and upperbandwidth l and u and with 1 on the band
+Eigen::MatrixXd bandMatrix(int r, int c, int l, int u, SymmetricStorage sym)
+{
+  if(l == Size::inf)
+    l /= 2; // to avoid overflow
+  if(u == Size::inf)
+    u /= 2; // to avoid overflow
+  // Create a band matrix with 0 on the band and -1 outside
+  Eigen::MatrixXd B(r, c);
+  B.setOnes();
+  for(int i = 0; i < r; ++i)
+  {
+    for(int j = 0; j < c; ++j)
+    {
+      if(j < i - l || j > i + u)
+        B(i, j) = 0;
+    }
+  }
+  if(sym == SymmetricStorage::Lower)
+  {
+    for(int i = 0; i < r; ++i)
+    {
+      for(int j = i + 1; j < c; ++j)
+        B(i, j) = 0;
+    }
+  }
+  else if(sym == SymmetricStorage::Upper)
+  {
+    for(int i = 0; i < r; ++i)
+    {
+      for(int j = 0; j < i; ++j)
+        B(i, j) = 0;
+    }
+  }
+
+  return B;
+}
+
+// Based on the output of bandMatrix, generates a matrix where the values are
+//  - for elements inside a band (le,ue): the storage index of this element
+//  - for element outside the band: -1
+// le and ue must be the effective upper and lower bandwidth, as computed by StorageScheme::setShape
+Eigen::MatrixXd indexedBandMatrix(Eigen::MatrixXd B, int le, int ue, SymmetricStorage sym)
+{
+  const int r = B.rows();
+  const int c = B.cols();
+
+  int idx = 1;
+  for(int j = 0; j < c; ++j)
+  {
+    // if empty col, we skip the iteration
+    if(B.col(j).sum() == 0)
+      continue;
+    for(int i = j - ue; i <= j + le; ++i)
+    {
+      if(0 <= i && i < r)
+        B(i, j) = idx;
+      ++idx;
+    }
+  }
+  B.array() -= 1;
+
+  // copying the symmetric part
+  if(sym == SymmetricStorage::Lower)
+  {
+    for(int i = 0; i < r; ++i)
+    {
+      for(int j = i + 1; j < c; ++j)
+        B(i, j) = B(j, i);
+    }
+  }
+  else if(sym == SymmetricStorage::Upper)
+  {
+    for(int i = 0; i < r; ++i)
+    {
+      for(int j = 0; j < i; ++j)
+        B(i, j) = B(j, i);
+    }
+  }
+
+  return B;
+}
+
+void testBandShape(int r, int c, int l, int u, SymmetricStorage sym)
+{
+  Eigen::MatrixXd B = bandMatrix(r, c, l, u, sym);
+  BandShape shape(r, c, l, u);
+  std::unique_ptr<StorageScheme> storage = std::make_unique<BandStorageScheme>(sym);
+  storage->setShape(shape);
+
+  // Test effective bandwidth
+  int le = static_cast<const BandStorageScheme &>(*storage).effectiveLowerBandwidth();
+  int ue = static_cast<const BandStorageScheme &>(*storage).effectiveUpperBandwidth();
+  Eigen::MatrixXd C = bandMatrix(r, c, le, ue, sym);
+  FAST_CHECK_EQ(B, C);
+
+  Eigen::MatrixXd I = indexedBandMatrix(B, le, ue, sym);
+
+  for(int i = 0; i < r; ++i)
+  {
+    for(int j = 0; j < c; ++j)
+    {
+      const auto & res = storage->index(i, j);
+      FAST_CHECK_EQ(res.first, I(i, j));
+      if(res.first >= 0)
+      {
+        switch(sym)
+        {
+          case SymmetricStorage::None:
+            FAST_CHECK_UNARY_FALSE(res.second);
+            break;
+          case SymmetricStorage::Lower:
+            FAST_CHECK_EQ(res.second, j > i);
+            break;
+          case SymmetricStorage::Upper:
+            FAST_CHECK_EQ(res.second, j < i);
+            break;
+          default:
+            assert(false);
+        }
+      }
+    }
+  }
+}
+
+TEST_CASE("Band storage")
+{
+  constexpr SymmetricStorage None = SymmetricStorage::None;
+  constexpr SymmetricStorage Lower = SymmetricStorage::Lower;
+  constexpr SymmetricStorage Upper = SymmetricStorage::Upper;
+  constexpr int inf = Size::inf;
+  for(int l = -8; l <= 8; ++l)
+  {
+    for(int u = -l; u <= 8; ++u)
+    {
+      testBandShape(7, 7, l, u, None);  // square
+      testBandShape(4, 7, l, u, None);  // wide
+      testBandShape(7, 4, l, u, None);  // tall
+    }
+  }
+  for(int i = -8; i <= 8; ++i)
+  {
+    testBandShape(7, 7, inf, i, None); // square
+    testBandShape(4, 7, inf, i, None); // wide
+    testBandShape(7, 4, inf, i, None); // tall
+    testBandShape(7, 7, i, inf, None);
+    testBandShape(4, 7, i, inf, None);
+    testBandShape(7, 4, i, inf, None); 
+  }
+
+  // Symmetric
+  for(int i = 0; i < 8; ++i)
+  {
+    testBandShape(7, 7, i, i, Lower);
+    testBandShape(7, 7, i, i, Upper);
+  }
+  testBandShape(7, 7, inf, inf, Lower);
+  testBandShape(7, 7, inf, inf, Upper);
+}
 
 TEST_CASE("Dense storage iterators")
 {
@@ -16,29 +179,60 @@ TEST_CASE("Dense storage iterators")
 
   auto row = storage->row(2);
   auto it = row.begin();
-  FAST_CHECK_EQ(*it, 2);
+  FAST_CHECK_EQ(*it, LIV{0, 2, false});
   ++it;
-  FAST_CHECK_EQ(*it, 7);
+  FAST_CHECK_EQ(*it, LIV{1, 7, false});
   ++it;
-  FAST_CHECK_EQ(*it, 12);
+  FAST_CHECK_EQ(*it, LIV{2, 12, false});
   ++it;
-  FAST_CHECK_EQ(*it, 17);
+  FAST_CHECK_EQ(*it, LIV{3, 17, false});
   ++it;
-  FAST_CHECK_EQ(*it, 22);
+  FAST_CHECK_EQ(*it, LIV{4, 22, false});
   ++it;
-  FAST_CHECK_EQ(*it, 27);
+  FAST_CHECK_EQ(*it, LIV{5, 27, false});
   ++it;
-  FAST_CHECK_EQ(*it, 32);
+  FAST_CHECK_EQ(*it, LIV{6, 32, false});
   ++it;
-  FAST_CHECK_EQ(*it, 37);
+  FAST_CHECK_EQ(*it, LIV{7, 37, false});
   ++it;
-  FAST_CHECK_EQ(*it, -1);
+  FAST_CHECK_EQ(*it, LIV{});
   FAST_CHECK_EQ(it, row.end());
 
-  int i = 15;
-  for(auto k : storage->col(3))
+  int i = 0;
+  int idx = 15;
+  for(const auto & k : storage->col(3))
   {
-    FAST_CHECK_EQ(k, i);
+    FAST_CHECK_EQ(k, LIV{i, idx, false});
     ++i;
+    ++idx;
+  }
+}
+
+TEST_CASE("Band storage iterators")
+{
+  BandShape shape(5, 9, 1, 2); // Same matrix as in doc of BandStorageScheme
+  std::unique_ptr<StorageScheme> storage = std::make_unique<BandStorageScheme>();
+  storage->setShape(shape);
+
+  auto row = storage->row(3);
+  auto it = row.begin();
+  FAST_CHECK_EQ(*it, LIV{2, 11, false});
+  ++it;
+  FAST_CHECK_EQ(*it, LIV{3, 14, false});
+  ++it;
+  FAST_CHECK_EQ(*it, LIV{4, 17, false});
+  ++it;
+  FAST_CHECK_EQ(*it, LIV{5, 20, false});
+  ++it;
+  FAST_CHECK_EQ(*it, LIV{});
+  FAST_CHECK_EQ(it, row.end());
+
+  int i = 1;
+  int idx = 12;
+  for(const auto & k : storage->col(3))
+  {
+    FAST_CHECK_EQ(k, LIV{i, idx, false});
+    ++i;
+    ++idx;
   }
 }
